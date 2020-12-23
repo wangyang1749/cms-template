c0f2480f100cb314d7c1c90f0b0555adab798278
wangyang1749 <1749748955@qq.com>
2020-12-23 20:54:56 +0800

    update

--- resources/js/write.js
+++ resources/js/write.js
@@ -1,3 +1,35 @@
+/**
+* 插入内容到光标处
+*/
+(function ($) {
+    // "use strict";
+    $.fn.extend({
+        insertAtCaret: function (myValue) {
+            var $t = $(this)[0];
+            if (document.selection) {
+                this.focus();
+                var sel = document.selection.createRange();
+                sel.text = myValue;
+                this.focus();
+            } else {
+                if ($t.selectionStart || $t.selectionStart == '0') {
+                    var startPos = $t.selectionStart;
+                    var endPos = $t.selectionEnd;
+                    var scrollTop = $t.scrollTop;
+                    $t.value = $t.value.substring(0, startPos) + myValue + $t.value.substring(endPos, $t.value.length);
+                    this.focus();
+                    $t.selectionStart = startPos + myValue.length;
+                    $t.selectionEnd = startPos + myValue.length;
+                    $t.scrollTop = scrollTop;
+                } else {
+                    this.value += myValue;
+                    this.focus();
+                }
+            }
+        }
+    });
+})(jQuery);
+
 
 function cmsWriteInit() {
     loadCategory()
@@ -70,6 +102,7 @@ function showTags() {
                     data: JSON.stringify({ "name": tag, "slugName": tag }),
                     success: function (data) {
                         // console.log(data.data.id)
+                        
                         cmsWrite.selectTags.push(data.data.id)
                         selectTagsMap[data.data.name] = data.data.id
                         // console.log(cmsWrite.selectTags)
